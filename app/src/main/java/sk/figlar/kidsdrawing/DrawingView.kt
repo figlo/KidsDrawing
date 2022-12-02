01c83e40f7cd6b112108063e5fdf2e25213aeb9b
figlo <miro@figlar.sk>
2022-12-02 12:47:55 +0100

    preparing the BrushSize Selector

--- app/src/main/java/sk/figlar/kidsdrawing/DrawingView.kt
+++ app/src/main/java/sk/figlar/kidsdrawing/DrawingView.kt
@@ -3,6 +3,7 @@ package sk.figlar.kidsdrawing
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,6 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint.strokeCap = Paint.Cap.ROUND
         mDrawPath = CustomPath(mColor, mBrushSize)
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20f
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -88,6 +88,15 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         invalidate()
 
         return true
+    }
+
+    fun setBrushSize(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint.strokeWidth = mBrushSize
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
--- app/src/main/java/sk/figlar/kidsdrawing/MainActivity.kt
+++ app/src/main/java/sk/figlar/kidsdrawing/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawingView: DrawingView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawingView)
+        drawingView.setBrushSize(20f)
     }
 }
