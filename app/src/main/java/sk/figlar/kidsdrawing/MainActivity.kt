3b117037cfdf43e80ee47939fc800e2b653299a7
figlo <miro@figlar.sk>
2022-12-03 22:24:31 +0100

    brush size dialog

--- app/src/main/java/sk/figlar/kidsdrawing/MainActivity.kt
+++ app/src/main/java/sk/figlar/kidsdrawing/MainActivity.kt
@@ -1,7 +1,9 @@
 package sk.figlar.kidsdrawing
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,36 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawingView)
         drawingView.setBrushSize(20f)
+
+        val ibBrush: ImageButton = findViewById(R.id.ibBrush)
+        ibBrush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ibSmallBrush)
+        smallBtn.setOnClickListener {
+            drawingView.setBrushSize(10f)
+            brushDialog.dismiss()
+        }
+
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ibMediumBrush)
+        mediumBtn.setOnClickListener {
+            drawingView.setBrushSize(20f)
+            brushDialog.dismiss()
+        }
+
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ibLargeBrush)
+        largeBtn.setOnClickListener {
+            drawingView.setBrushSize(30f)
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
