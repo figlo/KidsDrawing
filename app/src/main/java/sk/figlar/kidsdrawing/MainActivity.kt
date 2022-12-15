74ba66888dab4b33261e82b208c8e2a19f81c1d4
figlo <miro@figlar.sk>
2022-12-15 13:01:32 +0100

    adding a share feature

--- app/src/main/java/sk/figlar/kidsdrawing/MainActivity.kt
+++ app/src/main/java/sk/figlar/kidsdrawing/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -241,6 +242,7 @@ class MainActivity : AppCompatActivity() {
                                 "File saved successfully: $result",
                                 Toast.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         } else {
                             Toast.makeText(
                                 this@MainActivity,
@@ -267,6 +269,17 @@ class MainActivity : AppCompatActivity() {
     private fun cancelProgressDialog() {
         if (customProgressDialog != null) {
             customProgressDialog.dismiss()
+        }
+    }
+
+    private fun shareImage(result: String) {
+        MediaScannerConnection.scanFile(this, arrayOf(result), null) {
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
         }
     }
 }
